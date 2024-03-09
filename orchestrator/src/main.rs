f49f6a1f0badc967ecacacaa84af76edf2709a17
AlessioZeni <alessio.zeni01@gmail.com>
2024-03-09 17:14:51 +0100

    removed cli

--- orchestrator/src/main.rs
+++ orchestrator/src/main.rs
@@ -1,3 +1,23 @@
+use std::thread;
+
+use queue::QueueHandler;
+use state::StateHandler;
+
+mod action;
+mod api;
+mod queue;
+mod state;
+
 fn main() {
-    println!("Hello, world!");
+    let state_handler = StateHandler::new();
+    let queue_handler = QueueHandler::new(state_handler);
+
+    let queue_handler_clone = queue_handler.clone();
+    let queue_handler_thread = thread::spawn(move || {
+        queue_handler_clone.main_loop()
+    });
+
+    // TODO instantiate API with ref to state_handler and queue
+
+    queue_handler_thread.join().unwrap();
 }
