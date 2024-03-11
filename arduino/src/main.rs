d2507bebf850604d20caca95d1b1a05cc48090d8
AlessioZeni <alessio.zeni01@gmail.com>
2024-03-11 08:44:53 +0100

    ordine

--- arduino/src/main.rs
+++ arduino/src/main.rs
@@ -1,27 +1,24 @@
 #![no_std]
 #![no_main]
 
-use panic_halt as _;
 
+use panic_halt as _;
+use serialmessage::SerMsg;
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
 
     let mut led = pins.d13.into_output();
 
     loop {
         led.toggle();
+        
+        //let send_data_vec= [1, 2, 3, 4];
+        //let send_msg = SerMsg::create_msg_arr(&send_data_vec, 1).unwrap();
+        
+        //arduino_hal::delay_ms(send_msg.1 as u16);
         arduino_hal::delay_ms(1000);
     }
 }
