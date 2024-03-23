21bded1608244d7b94de31e066f0686f7ed33b4e
Stypox <stypox@pm.me>
2024-03-23 18:40:47 +0100

    Properly implement queue without spin lock

--- orchestrator/src/queue/mod.rs
+++ orchestrator/src/queue/mod.rs
@@ -1,38 +1,59 @@
-use std::sync::{Arc, Mutex};
+use std::{collections::VecDeque, sync::{Arc, Condvar, Mutex}};
 
 use crate::{action::Action, state::StateHandler};
 
 #[derive(Debug)]
 struct Queue {
-    actions: Vec<Box<dyn Action>>,
-    current_action: Option<Box<dyn Action>>,
-    state_handler: StateHandler,
+    actions: VecDeque<Box<dyn Action>>,
+    paused: bool,
 }
 
 #[derive(Debug, Clone)]
 pub struct QueueHandler {
-    queue: Arc<Mutex<Queue>>,
+    queue: Arc<(Mutex<Queue>, Condvar)>,
+    state_handler: StateHandler,
     // TODO add serial object
 }
 
 impl QueueHandler {
     pub fn new(state_handler: StateHandler) -> QueueHandler {
         QueueHandler {
-            queue: Arc::new(Mutex::new(Queue {
-                actions: vec![],
-                current_action: None,
-                state_handler,
-            })),
+            queue: Arc::new((Mutex::new(Queue {
+                actions: VecDeque::new(),
+                paused: false,
+            }), Condvar::new())),
+            state_handler,
+        }
+    }
+
+    fn get_current_action(&self, mut last_current_action: Option<Box<dyn Action>>) -> Box<dyn Action> {
+        let (queue, condvar) = &*self.queue;
+        let mut queue = queue.lock().unwrap();
+
+        loop {
+            if queue.paused {
+                if let Some(current_action) = last_current_action {
+                    queue.actions.push_front(current_action);
+                    last_current_action = None;
+                }
+            } else if let Some(current_action) = last_current_action {
+                return current_action
+            } else if let Some(current_action) = queue.actions.pop_front() {
+                return current_action
+            }
+
+            queue = condvar.wait(queue).unwrap();
         }
     }
 
     pub fn main_loop(&self) {
+        let mut last_current_action = None;
         loop {
-            let mut queue = self.queue.lock().unwrap();
-            if let Some(current_action) = &mut queue.current_action {
-                if !current_action.step() {
-                    queue.current_action = None
-                }
+            let mut current_action = self.get_current_action(last_current_action);
+            if current_action.step() {
+                last_current_action = Some(current_action);
+            } else {
+                last_current_action = None;
             }
             // TODO
         }
