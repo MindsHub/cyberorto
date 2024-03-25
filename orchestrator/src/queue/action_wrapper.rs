92b4f0a3f56277e346fb2eacaf1a38225284ef1c
Stypox <stypox@pm.me>
2024-03-25 15:44:39 +0100

    Better way to implement queue

--- orchestrator/src/queue/action_wrapper.rs
+++ orchestrator/src/queue/action_wrapper.rs
@@ -4,6 +4,18 @@ pub type ActionId = u32;
 
 #[derive(Debug)]
 pub struct ActionWrapper {
-    pub action: Box<dyn Action>,
+    pub action: Option<Box<dyn Action>>, // will be None if this is a placeholder
     pub id: ActionId,
+}
+
+impl ActionWrapper {
+    pub fn make_placeholder_and_extract(&mut self) -> ActionWrapper {
+        assert!(self.action.is_some()); // can't make a placeholder... a placeholder
+        let mut new_action_wrapper = ActionWrapper {
+            action: None,
+            id: self.id,
+        };
+        std::mem::swap(self, &mut new_action_wrapper);
+        new_action_wrapper
+    }
 }
--- orchestrator/src/queue/mod.rs
+++ orchestrator/src/queue/mod.rs
@@ -23,7 +23,7 @@ struct Queue {
 
 impl Queue {
     fn create_action_wrapper(&mut self, action: Box<dyn Action>) -> ActionWrapper {
-        let res = ActionWrapper { action, id: self.id_counter };
+        let res = ActionWrapper { action: Some(action), id: self.id_counter };
         self.id_counter = self.id_counter.wrapping_add(1);
         res
     }
@@ -61,24 +61,43 @@ impl QueueHandler {
         }
     }
 
-    fn get_current_action(&self, mut last_current_action: Option<ActionWrapper>) -> ActionWrapper {
+    fn get_current_action(&self, last_current_action: Option<ActionWrapper>) -> ActionWrapper {
         let (queue, condvar) = &*self.queue;
         let mut queue = queue.lock().unwrap();
 
+        // Readd the last current action to the queue at the position where its placeholder is.
+        // This allows moving even the action being currently executed.
+        if let Some(action) = last_current_action {
+            if action.action.is_some() {
+                if let Some(item) = queue.actions.iter_mut().find(|item| item.id == action.id) {
+                    // If the placeholder corresponding to the current action is in the queue,
+                    // replace it with the non-placeholder current action. This not only moves
+                    // the Action object back in the queue, but also updates other fields in
+                    // ActionWrapper and effectively pauses the action if it's not going to be
+                    // taken again right after in the loop below.
+                    *item = action;
+                }
+                // `else`, it means that the placeholder has been deleted from the queue
+                // in the meantime, so just let the current action be dropped. The loop below
+                // will decide which action will come next.
+            } else if let Some(index) = queue.actions.iter().position(|item| item.id == action.id) {
+                // If the current action has finished executing,
+                // remove its corresponding placeholder from the queue.
+                queue.actions.remove(index);
+            }
+            // `else`, the current action has finished executing and its corresponding
+            // placeholder has already been deleted from the queue, so nothing to do
+        }
+
         loop {
             if queue.paused || queue.emergency != EmergencyStatus::None {
-                if let Some(current_action) = last_current_action {
-                    queue.actions.push_front(current_action);
-                    last_current_action = None;
-                }
                 if queue.emergency == EmergencyStatus::WaitingForReset {
                     queue.emergency = EmergencyStatus::Resetting;
                     return queue.create_action_wrapper(Box::new(EmergencyAction {}));
                 }
-            } else if let Some(current_action) = last_current_action {
-                return current_action
-            } else if let Some(current_action) = queue.actions.pop_front() {
-                return current_action
+
+            } else if let Some(current_action) = queue.actions.front_mut() {
+                return current_action.make_placeholder_and_extract()
             }
 
             queue = condvar.wait(queue).unwrap();
@@ -86,14 +105,14 @@ impl QueueHandler {
     }
 
     pub fn main_loop(&self) {
-        let mut last_current_action = None;
+        let mut last_action = None; // will be None only the first iteration
         loop {
-            let mut current_action = self.get_current_action(last_current_action);
-            if current_action.action.step(&self.state_handler) {
-                last_current_action = Some(current_action);
-            } else {
-                last_current_action = None;
+            let mut action = self.get_current_action(last_action);
+            // unwrapping since the returned action can't be a placeholder
+            if !action.action.as_mut().unwrap().step(&self.state_handler) {
+                action.action = None;
             }
+            last_action = Some(action);
         }
     }
 
