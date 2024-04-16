9518caac58f5fe746da4f3d461bcaa9cfb91f994
AlessioZeni <alessio.zeni01@gmail.com>
2024-04-16 10:02:21 +0200

    better updater

--- debian-installation.sh
+++ debian-installation.sh
@@ -4,11 +4,16 @@ sudo apt update -y
 sudo apt upgrade -y
 #install rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
 #avr-hal dependencies:
-sudo apt install avr-libc gcc-avr pkg-config avrdude libudev-dev build-essential
+sudo apt install avr-libc gcc-avr pkg-config avrdude libudev-dev build-essential -y
 #vs-codium
 #sudo apt install codium
 #install ravedude
 cargo +stable install ravedude
-#check test
+#check build arduino
+cd arduino
+cargo build --release
+cd ..
+#build and test orchestrator
 cargo test
