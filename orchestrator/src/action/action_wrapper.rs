6424b6e2c7c80ed6551f7fad2a9c761ef9c706ac
Stypox <stypox@pm.me>
2024-04-18 14:03:14 +0200

    Add docs to ActionWrapper

--- orchestrator/src/action/action_wrapper.rs
+++ orchestrator/src/action/action_wrapper.rs
@@ -3,17 +3,35 @@ use std::{any::TypeId, fmt::format, fs::create_dir_all, path::{Path, PathBuf}};
 use super::{command_list::CommandListAction, emergency::EmergencyAction, Action};
 
 
-
+/// Holds an `Action` along with some fixed stats needed to keep track of
+/// its execution and/or save it to disk. Can act as a placeholder in the
+/// queue, so if the `Action` is being executed, the `ActionWrapper` in the
+/// queue will have [`action`](ActionWrapper::action)`= None`.
 #[derive(Debug)]
 pub struct ActionWrapper {
-    pub action: Option<Box<dyn Action>>, // will be None if this is a placeholder
+    /// The action being wrapped.
+    /// Will be None if this ActionWrapper is a placeholder.
+    pub action: Option<Box<dyn Action>>,
+
+    /// Additional information to identify the action and save it to disk.
     pub ctx: Context,
 }
 
+/// Holds information to identify an action and load/save it from/to disk.
 #[derive(Debug, Clone)]
 pub struct Context {
+    /// A **unique** identifier for the action.
+    /// Will be distinct for each action instance.
     id: ActionId,
+
+    /// The name of the type of the action. Obtained from a call to
+    /// [`Action::get_type_name()`](Action::get_type_name()) on an
+    /// `Action`` implementor.
     type_name: String,
+
+    /// The directory in which to store files about this action, both during
+    /// execution (e.g. to cache images/data for later usage) or because the
+    /// action is being saved to disk.
     save_dir: PathBuf,
 }
 
@@ -21,6 +39,14 @@ pub type ActionId = u32;
 
 
 impl ActionWrapper {
+    /// Creates a non-placeholder `ActionWrapper`, initializing the context with
+    /// `A::get_type_name()`.
+    ///
+    /// * `action` the action to wrap (although in the end a `dyn Action` is
+    ///            stored in the wrapper, its type is needed at compile time to
+    ///            call the correct
+    ///            [`Action::get_type_name()`](Action::get_type_name()))
+    /// * `id` a **unique** ID for this action
     pub fn new<A: Action + 'static>(action: A, id: ActionId) -> ActionWrapper {
         ActionWrapper {
             action: Some(Box::new(action)),
@@ -32,10 +58,18 @@ impl ActionWrapper {
         }
     }
 
+    /// Returns the **unique** ID of the action being wrapped.
     pub fn get_id(&self) -> ActionId {
         self.ctx.id
     }
 
+    /// Returns an `Action` after loading it from disk at the location `dir`,
+    /// or an error if something went wrong. Chooses the type of the action
+    /// to load based on the TYPENAME stored in `dir`'s name.
+    ///
+    /// * `dir` is where to look for the files for the `Action` to load. The
+    ///         name must be of the form `ID_TYPENAME`, and ID and TYPENAME
+    ///         will be extracted from there.
     pub fn load_from_disk(dir: &Path) -> Result<ActionWrapper, String> {
         if !dir.is_dir() {
             return Err(format!("Not a directory: {dir:?}"))
@@ -59,6 +93,8 @@ impl ActionWrapper {
         }
     }
 
+    /// Saves this `Action` to disk, by storing files inside `self.ctx.save_dir`.
+    /// Returns an error if something went wrong.
     pub fn save_to_disk(&self) -> Result<(), String> {
         let action = if let Some(action) = &self.action {
             action
@@ -74,6 +110,9 @@ impl ActionWrapper {
 }
 
 impl Context {
+    /// Returns the directory in which to store files about this action, both
+    /// during execution (e.g. to cache images/data for later usage) or when
+    /// saving the action to disk.
     pub fn get_save_dir(&self) -> &PathBuf {
         &self.save_dir
     }
