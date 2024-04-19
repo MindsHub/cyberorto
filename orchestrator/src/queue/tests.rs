93ec5d366f1789b74862a2973dd39f32fd884e18
Stypox <stypox@pm.me>
2024-04-19 09:10:37 +0200

    Improve state/queue tests setup

--- orchestrator/src/queue/tests.rs
+++ orchestrator/src/queue/tests.rs
@@ -5,46 +5,38 @@ use std::{thread::{self, JoinHandle}, time::Duration};
 use serialport::TTYPort;
 
 use super::*;
-use crate::state::{fake_slave_bot::FakeSlaveBotData, tests::TestState};
+use crate::state::{fake_slave_bot::FakeSlaveBotData, tests::{get_test_state, TestState}};
 
 pub struct TestQueue {
-    pub state_handler: StateHandler,
-    pub slave_bot_join_handle: JoinHandle<()>,
-    pub slave_bot_data: Arc<Mutex<FakeSlaveBotData>>,
     pub queue_handler: QueueHandler,
     pub save_dir: PathBuf,
     pub queue_join_handle: JoinHandle<()>,
 }
 
-impl Default for TestQueue {
-    fn default() -> Self {
-        let TestState {
-            state_handler,
-            slave_bot_join_handle,
-            slave_bot_data,
-        } = TestState::default();
+pub fn get_test_state_queue() -> (TestState, TestQueue) {
+    let test_state = get_test_state();
 
-        let save_dir = tempdir::TempDir::new("cyberorto_test").unwrap().into_path();
-        let queue_handler = QueueHandler::new(state_handler.clone(), save_dir.clone());
-        let queue_handler_clone = queue_handler.clone();
-        let join_handle = std::thread::spawn(move || { queue_handler_clone.run() });
+    let save_dir = tempdir::TempDir::new("cyberorto_test").unwrap().into_path();
+    let queue_handler = QueueHandler::new(test_state.state_handler.clone(), save_dir.clone());
+    let queue_handler_clone = queue_handler.clone();
+    let queue_join_handle = std::thread::spawn(move || { queue_handler_clone.run() });
 
+    (
+        test_state,
         TestQueue {
-            state_handler,
-            slave_bot_join_handle,
-            slave_bot_data,
             queue_handler,
             save_dir,
-            queue_join_handle: join_handle,
+            queue_join_handle,
         }
-    }
+    )
 }
 
-#[test]
-fn queue_stops() {
-    let TestQueue { queue_handler, queue_join_handle, .. } = TestQueue::default();
 
-    queue_handler.stop();
+#[test]
+fn test_queue_stops() {
+    let (_, q) = get_test_state_queue();
+
+    q.queue_handler.stop();
     thread::sleep(Duration::from_millis(50));
-    assert!(queue_join_handle.is_finished());
+    assert!(q.queue_join_handle.is_finished());
 }
--- orchestrator/src/state/tests.rs
+++ orchestrator/src/state/tests.rs
@@ -16,42 +16,37 @@ pub struct TestState {
     pub slave_bot_data: Arc<Mutex<FakeSlaveBotData>>,
 }
 
-impl Default for TestState {
-    fn default() -> Self {
-        let (mut master, slave) = TTYPort::pair().expect("Unable to create tty pair");
+pub fn get_test_state() -> TestState {
+    let (mut master, slave) = TTYPort::pair().expect("Unable to create tty pair");
 
-        let mut slave_bot = FakeSlaveBot::new(slave, *FAKE_BOT_NAME);
-        let slave_bot_data = slave_bot.get_data_ref();
-        let slave_bot_join_handle = std::thread::spawn(move || {
-            tokio::runtime::Builder::new_current_thread()
-                .enable_all()
-                .build()
-                .unwrap()
-                .block_on(async {
-                    slave_bot.run().await
-                });
-        });
+    let mut slave_bot = FakeSlaveBot::new(slave, *FAKE_BOT_NAME);
+    let slave_bot_data = slave_bot.get_data_ref();
+    let slave_bot_join_handle = std::thread::spawn(move || {
+        tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(async {
+                slave_bot.run().await
+            });
+    });
 
-        TestState {
-            state_handler: StateHandler::new(master),
-            slave_bot_join_handle,
-            slave_bot_data,
-        }
+    TestState {
+        state_handler: StateHandler::new(master),
+        slave_bot_join_handle,
+        slave_bot_data,
     }
 }
 
+
 #[tokio::test]
 async fn test_toggle_led() {
-    let TestState {
-        state_handler,
-        slave_bot_join_handle,
-        slave_bot_data,
-    } = TestState::default();
+    let s = get_test_state();
 
-    state_handler.toggle_led().await;
+    s.state_handler.toggle_led().await;
 
     assert_eq!(
         vec![Message::SetLed { led: true }],
-        slave_bot_data.lock().unwrap().received_messages,
+        s.slave_bot_data.lock().unwrap().received_messages,
     );
 }
