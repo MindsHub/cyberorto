eb98e8bad2004e38ab15af8dbeb6bfb73ede0fac
Stypox <stypox@pm.me>
2024-04-20 15:50:19 +0200

    Add serial fmt to anemometro

--- anemometro/src/main.rs
+++ anemometro/src/main.rs
@@ -3,25 +3,11 @@
 #![feature(abi_avr_interrupt)]
 #![feature(noop_waker)]
 
-use core::{
-    future::Future,
-    pin::pin,
-    task::{Context, Waker},
-};
-
-use arduino_common::{no_std::SingleCoreMutex, traits:: MutexTrait, BotState, SlaveBot};
-
-
-use arduino_hal::port::{mode::Output, Pin, PinOps};
-
-use millis::{init_millis, millis, Wait};
+use millis::init_millis;
 use panic_halt as _;
-use serial_hal::SerialHAL;
 
 /// module containing all timings stuff (init, millis, micros...)
 pub mod millis;
-/// module containing all serial stugg (init, async traits, buffer dimensions...)
-pub mod serial_hal;
 
 
 ///Main entry point
@@ -29,21 +15,15 @@ pub mod serial_hal;
 fn main() -> ! {
     //getting peripherals
     let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = ardUino_hal::pins!(dp);
-
-    // set led pin to low
-    let mut led = pins.d13.into_output();
-    led.set_low();
-    // extract usart, and init it
-    let serial = dp.USART0;
-    let serial = SerialHAL::new(serial);
-    //let com = Comunication::new(serial);
+    let pins = arduino_hal::pins!(dp);
+    let pin_ane = pins.d1.into_floating_input();
+    let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
     //enable interrupts
     unsafe { avr_device::interrupt::enable() };
-    
     init_millis(dp.TC0);
-    let pin_ane = pins.d1.into_floating_input();
-    
+
+    ufmt::uwriteln!(&mut serial, "{} bytes available", 53);
+
     fn calc_time() {
         let mut time: u64 = 0;
         while pin_ane.islow() {
