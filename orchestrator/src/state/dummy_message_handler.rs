e5f2c7a34d926977aadde981221913a3aa5f02ce
Stypox <stypox@pm.me>
2025-06-18 21:06:50 +0200

    Handle errors correctly in DummyMessageHandler

--- orchestrator/src/state/dummy_message_handler.rs
+++ orchestrator/src/state/dummy_message_handler.rs
@@ -1,8 +1,12 @@
 use std::time::Duration;
 
-use embedcore::{common::controllers::pid::{CalibrationMode, PidController}, protocol::cyber::{MessagesHandler, Response}, std::{get_fake_motor, FakeDriver, FakeEncoder}, EncoderTrait};
+use embedcore::{
+    common::controllers::pid::{CalibrationMode, PidController},
+    protocol::cyber::{MessagesHandler, Response},
+    std::{get_fake_motor, FakeDriver, FakeEncoder},
+    EncoderTrait,
+};
 use tokio::time::Instant;
-
 
 pub struct DummyMessageHandler {
     water_state: bool,
@@ -18,8 +22,25 @@ impl DummyMessageHandler {
     const METERS_TO_STEPS: f32 = 100000.0;
 
     pub fn new() -> DummyMessageHandler {
-        Self { water_state: false, lights_state: false, pump_state: false, plow_state: false, led_state: false, time_finished: Instant::now(),
-            motor: PidController::new(get_fake_motor(), 2.0, 2.0) }
+        Self {
+            water_state: false,
+            lights_state: false,
+            pump_state: false,
+            plow_state: false,
+            led_state: false,
+            time_finished: Instant::now(),
+            motor: PidController::new(get_fake_motor(), 2.0, 2.0),
+        }
+    }
+
+    fn update_time_finished(&mut self, ms: u64) -> Option<Response> {
+        match Instant::now().checked_add(Duration::from_millis(ms)) {
+            Some(t) => {
+                self.time_finished = t;
+                Some(Response::Wait { ms })
+            }
+            None => Some(Response::Debug(*b"duration +")),
+        }
     }
 }
 
@@ -29,14 +50,20 @@ impl MessagesHandler for DummyMessageHandler {
         Some(Response::Done)
     }
     async fn reset_motor(&mut self) -> Option<Response> {
-        self.motor.calibration(0, CalibrationMode::NoOvershoot).await;
+        self.motor
+            .calibration(0, CalibrationMode::NoOvershoot)
+            .await;
         Some(Response::Done)
     }
     async fn state(&mut self) -> Option<Response> {
-        Some(
-            Response::State { water: self.water_state, lights: self.lights_state, pump: self.pump_state, plow: self.plow_state, led: self.led_state,
-                motor_pos: (self.motor.motor.read() as f32) / Self::METERS_TO_STEPS }
-        )
+        Some(Response::State {
+            water: self.water_state,
+            lights: self.lights_state,
+            pump: self.pump_state,
+            plow: self.plow_state,
+            led: self.led_state,
+            motor_pos: (self.motor.motor.read() as f32) / Self::METERS_TO_STEPS,
+        })
     }
     async fn poll(&mut self) -> Option<Response> {
         if self.time_finished <= Instant::now() {
@@ -46,29 +73,26 @@ impl MessagesHandler for DummyMessageHandler {
             self.plow_state = false;
             Some(Response::Done)
         } else {
-            Some(Response::Wait { ms: (self.time_finished - Instant::now()).as_millis() as u64 })
+            Some(Response::Wait {
+                ms: (self.time_finished - Instant::now()).as_millis() as u64,
+            })
         }
     }
     async fn water(&mut self, ms: u64) -> Option<Response> {
         self.water_state = true;
-        // TODO yanking with ? is wrong here, return Response::Error instead
-        self.time_finished = Instant::now().checked_add(Duration::from_millis(ms))?;
-        Some(Response::Done)
+        self.update_time_finished(ms)
     }
     async fn lights(&mut self, ms: u64) -> Option<Response> {
         self.lights_state = true;
-        self.time_finished = Instant::now().checked_add(Duration::from_millis(ms))?;
-        Some(Response::Done)
+        self.update_time_finished(ms)
     }
     async fn pump(&mut self, ms: u64) -> Option<Response> {
         self.pump_state = true;
-        self.time_finished = Instant::now().checked_add(Duration::from_millis(ms))?;
-        Some(Response::Done)
+        self.update_time_finished(ms)
     }
     async fn plow(&mut self, ms: u64) -> Option<Response> {
         self.plow_state = true;
-        self.time_finished = Instant::now().checked_add(Duration::from_millis(ms))?;
-        Some(Response::Done)
+        self.update_time_finished(ms)
     }
     async fn set_led(&mut self, state: bool) -> Option<Response> {
         self.led_state = state;
